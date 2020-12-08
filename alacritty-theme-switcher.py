257df63f0b4922e0066ad20e899d9bf33b9a239d
Snekyy <snekyy@protonmail.com>
2020-12-08 10:44:38 +0300

    code quality

--- alacritty-theme-switcher.py
+++ alacritty-theme-switcher.py
@@ -2,56 +2,63 @@
 
 import os
 import yaml
-from argparse import ArgumentParser
+import argparse
 
 
-def show_themes(alacritty_dir):
-    files = os.listdir(path=alacritty_dir + "/themes")
-    print(f"Available themes {len(files)}:")
-    print("\n".join([file.split('.')[0] for file in files]))
+def showThemes():
+    # Get all files from themes directory
+    themes = os.listdir(path=alacritty_dir + "themes/")
+    print(f"Total themes found {len(themes)}:")
+    # Del extentions, sort by alphabet, print
+    for theme in sorted(map(lambda x: x.split(".")[0], themes)):
+        print(theme)
+
+def setTheme(theme):
+    if not theme+".yml" in os.listdir(path=alacritty_dir+"themes/"):
+        exit(f"error: theme {theme} doesn't exists")
+    with open(alacritty_dir + "alacritty.yml", "r") as cfg:
+        cfg = yaml.load(cfg, Loader=yaml.Loader)
+        del cfg["colors"]
+    with open(alacritty_dir + "themes/" + theme + ".yml", "r") as theme:
+        theme = yaml.load(theme, Loader=yaml.Loader)
+        cfg.update(theme)
+        new_file = open(alacritty_dir + "alacritty.yml", "w")
+        yaml.dump(cfg, new_file)
+        new_file.close()
+          
+        
+
+def main():
+    parser = argparse.ArgumentParser(description="Theme switcher \
+                                     for Alacirtty terminal emulator")
+    parser.add_argument("-l", "--list-themes", action="store_true",
+                        help="print list of available themes")
+    parser.add_argument("-s", "--set-theme", help="123") 
+    args = parser.parse_args()
+    
+    # Check alacritty config dir
+    global alacritty_dir
+    alacritty_dir = f"/home/{os.getlogin()}/.config/alacritty/" 
+    
+    if not os.path.exists(alacritty_dir):
+        exit(f"error: {alacritty_dir} doesn't exists")
+    if not os.path.exists(alacritty_dir + "alacritty.yml"):
+        exit(f"error: {alacirtty_dir}alacritty.yml doesn't exists")
+    if not os.path.exists(alacritty_dir + "themes/"):
+        exit(f"error: {alacritty_dir}themes/ doesn't exists")
+    
+    # Filter command line arguments 
+    # to avoid bugs
+    if not args.set_theme and not args.list_themes:
+        parser.error("Missing argument -l/-s/-h")
+    
+    if args.list_themes:
+        showThemes()
+        exit(0)
+    if args.set_theme:
+        setTheme(args.set_theme)
+        exit(0)
 
 
-def chg_theme(alacirtty_dir, theme):
-    cfg = open(alacritty_dir + "/alacritty.yml", "r")
-    cfg = yaml.load(cfg, Loader=yaml.Loader)
-    themes = os.listdir(path=alacritty_dir + "/themes")
-    if not theme + ".yml" in themes:
-        print(f'error: theme "{theme}" not found!')
-        exit(1)
-    del cfg["colors"]
-    theme = open(alacritty_dir + "/themes/" + theme + ".yml", "r")
-    theme = yaml.load(theme, Loader=yaml.Loader)
-    cfg.update(theme)  
-    new_file = open(alacritty_dir + "/alacritty.yml", "w")
-    yaml.dump(cfg, new_file)
-
-
-
-parser = ArgumentParser(description="Alacritty theme switcher")
-parser.add_argument("-s", "--set-theme", default=False,
-                    metavar="theme",type=str, help="set theme")
-parser.add_argument("-l", "--list-themes", action="store_true",
-                    help="show available themes")
-parser.add_argument("-b", "--backup", action="store_true",
-                    help="save current config to .bak file")
-args = parser.parse_args()
-
-alacritty_dir = f"/home/{os.getlogin()}/.config/alacritty"
-
-if not os.path.exists(alacritty_dir):
-    print("error: ~/.config/alacritty doesn't exists!")
-    exit(1)
-
-if not os.path.exists(alacritty_dir + "/themes"):
-    print("error ~/.config/alacritty/themes doesn't exists!")
-    exit(1)
-
-if args.backup:
-    os.system("cp ~/.config/alacritty/alacritty.yml \
-              ~/.config/alacritty/alacritty.yml.bak")
-
-if args.list_themes:
-    show_themes(alacritty_dir)
-
-if args.set_theme != False:
-    chg_theme(alacritty_dir, args.set_theme)
+if __name__ == "__main__":
+    main()
